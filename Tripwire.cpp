5a2a87d5cdbadb9e154254035b2864a7c20df7ed
Leenix <leenix48@gmail.com>
2017-03-14 14:39:31 +1000

    Bug fixing

--- Tripwire.cpp
+++ Tripwire.cpp
@@ -1,150 +1,154 @@
 #include "Tripwire.h"
 
 Tripwire::Tripwire(range_function get_range) {
-  /**
-  * Create a new tripwire object
-  * @param get_range Passed function to get the range of the function. Unitless,
-  * but must be a long.
-  */
-  if (get_range != NULL) {
-    _get_range = get_range;
-  }
+    /**
+    * Create a new tripwire object
+    * @param get_range Passed function to get the range of the function. Unitless,
+    * but must be a long.
+    */
+    if (get_range != NULL) {
+        _get_range = get_range;
+    }
 
-  is_calibrated = false;
-  distance = 0;
-  baseline_distance = 0;
-  baseline_variance = 0;
-  distance_threshold = DEFAULT_DISTANCE_THRESHOLD;
-  min_baseline_reads = DEFAULT_MIN_BASELINE_READS;
-  max_baseline_read = DEFAULT_MAX_BASELINE_READS;
-  max_baseline_variance = DEFAULT_MAX_BASELINE_VARIANCE;
-  baseline_read_interval = DEFAULT_BASELINE_READ_INTERVAL;
-  min_successive_detections = DEFAULT_MINIMUM_SUCCESSIVE_DETECTIONS;
+    is_calibrated = false;
+    distance = 0;
+    baseline_distance = 0;
+    baseline_variance = 0;
+    distance_threshold = DEFAULT_DISTANCE_THRESHOLD;
+    min_baseline_reads = DEFAULT_MIN_BASELINE_READS;
+    max_baseline_reads = DEFAULT_MAX_BASELINE_READS;
+    max_baseline_variance = DEFAULT_MAX_BASELINE_VARIANCE;
+    baseline_read_interval = DEFAULT_BASELINE_READ_INTERVAL;
+    min_successive_detections = DEFAULT_MINIMUM_SUCCESSIVE_DETECTIONS;
 }
 
-Tripwire::start(unsigned long threshold) {
-  /**
-  * Start the tripwire.
-  * Runtime variables are reset.
-  * update must be called regularly to ensure consistent reads
-  * @param distance_threshold Minimum reduction in distance to count a
-  * detection. Units are inhereted from the _get_range function.
-  */
-  _event_start_time = 0;
-  _last_event_width = 0;
-  _successive_detections = 0;
-  num_detections = 0;
+void Tripwire::start() {
+    /**
+    * Start the tripwire.
+    * Runtime variables are reset.
+    * update must be called regularly to ensure consistent reads
+    */
+    event_start_time = 0;
+    last_event_width = 0;
+    _successive_detections = 0;
+    num_detections = 0;
 
-  calibrate();
+    calibrate();
 }
 
 void Tripwire::calibrate() {
-  /**
-  * Calibrate the baseline distance of the sensor.
-  * Ensure nothing is moving in front of the sensor during calibration or the
-  * baseline will be inaccurate.
-  * is_calibrated will be updated after this function has completed to indicate
-  * whether the calibration was successful.
-  */
-  is_calibrated = false;
+    /**
+    * Calibrate the baseline distance of the sensor.
+    * Ensure nothing is moving in front of the sensor during calibration or the
+    * baseline will be inaccurate.
+    * is_calibrated will be updated after this function has completed to indicate
+    * whether the calibration was successful.
+    */
+    is_calibrated = false;
 
-  if (_get_range != NULL && _distance_threshold != NULL) {
-    baseline_distance = (*_get_range)();
-    baseline_variance = baseline_distance;
-    int baseline_reads = 1;
+    if (_get_range && distance_threshold) {
+        baseline_distance = (*_get_range)();
+        baseline_variance = baseline_distance;
+        int baseline_reads = 1;
 
-    // Keep reading in the baseline until it stablises
-    while (
-        (baseline_reads < min_baseline_reads || average_variance > variance) &&
-        baseline_reads < MAX_BASELINE_READS) {
-      long new_range = (*_get_range)();
-      long new_variance = abs(average - new_range);
+        // Keep reading in the baseline until it stablises or until the max reads are reached
+        while ((baseline_reads < min_baseline_reads || baseline_variance > max_baseline_variance) &&
+               baseline_reads < max_baseline_reads) {
+            long new_range = (*_get_range)();
+            long new_variance = abs(baseline_distance - new_range);
 
-      average_variance = ((average_variance + new_variance) / 2);
-      baseline_distance = ((average_range + new_range) / 2);
+            baseline_variance = ((baseline_variance + new_variance) / 2);
+            baseline_distance = ((baseline_distance + new_range) / 2);
 
-      baseline_reads++;
-      delay(baseline_read_interval);
+            baseline_reads++;
+            delay(baseline_read_interval);
+        }
+
+        // Calibration fails if the range is varying too much
+        if (baseline_variance < max_baseline_variance) {
+            is_calibrated = true;
+        }
     }
-
-    // Calibration fails if the range is varying too much
-    if (average_variance < variance) {
-      is_calibrated = true;
-    }
-  }
 }
 
 void Tripwire::update() {
-  /**
-  * Update the state of the tripwire
-  * This function should be called regularly to ensure data is accurate.
-  * Event callbacks are called from this function.
-  */
-  distance = (&_get_range)();
+    /**
+    * Update the state of the tripwire
+    * This function should be called regularly to ensure data is accurate.
+    * Event callbacks are called from this function.
+    */
+    distance = (*_get_range)();
 
-  // Detection occurs when target breaks the LoS to the baseline
-  if ((baseline_distance - distance) > distance_threshold) {
+    // Detection occurs when target breaks the LoS to the baseline
+    if ((baseline_distance - distance) > distance_threshold) {
+        // An event starts if the minimum number of successive detections has been
+        // reached. Only one event is recorded per "trip"
+        if (_successive_detections == min_successive_detections) {
+            _successive_detections += 1;
+            event_start_time = millis();
+            num_detections++;
 
-    // An event starts if the minimum number of successive detections has been
-    // reached. Only one event is recorded per "trip"
-    if (_successive_detections == min_successive_detections) {
-      _successive_detections += 1;
-      event_start_time = millis();
-      num_detections++;
+            // Call the event start callback if it's been set
+            if (_event_start) {
+                (*_event_start)();
+            }
+        }
 
-      // Call the event start callback if it's been set
-      if (_event_start) {
-        (*_event_start)();
-      }
+        // Not enough successive detections yet
+        else if (_successive_detections < min_successive_detections) {
+            _successive_detections += 1;
+        }
     }
 
-    // Not enough successive detections yet
-    else if (_successive_detections < min_successive_detections) {
-      _successive_detections += 1;
-    }
-  }
+    // Nothing detected. End any active events and reset tripwire.
+    else {
+        if (_successive_detections > 0) {
+            last_event_width = millis() - event_start_time;
 
-  // Nothing detected. End any active events and reset tripwire.
-  else {
-    if (_successive_detections > 0) {
-      last_event_width = millis() - event_start_time;
-
-      // Call the event end callback if it has been set
-      if (_event_end) {
-        (*_event_end)();
-      }
+            // Call the event end callback if it has been set
+            if (_event_end) {
+                (*_event_end)();
+            }
+        }
+        _successive_detections = 0;
     }
-    _successive_detections = 0;
-  }
 }
 
 void Tripwire::set_range_function(range_function function) {
-  /**
-  * Set the _get_range function
-  * @param function The external function that returns the recorded distance of
-  * the sensor
-  */
-  if (function != NULL) {
-    _get_range = function;
-  }
+    /**
+    * Set the _get_range function
+    * @param function The external function that returns the recorded distance of
+    * the sensor
+    */
+    if (function != NULL) {
+        _get_range = function;
+    }
 }
 
 void Tripwire::set_event_start_callback(event_callback callback) {
-  /**
-  * Set the event start callback function
-  * @param callback Function to call when an event starts
-  */
-  if (callback != NULL) {
-    _event_start = callback;
-  }
+    /**
+    * Set the event start callback function
+    * @param callback Function to call when an event starts
+    */
+    if (callback != NULL) {
+        _event_start = callback;
+    }
 }
 
 void Tripwire::set_event_end_callback(event_callback callback) {
-  /**
-  * Set the event end callback function
-  * @param callback Function to call when an event ends
-  */
-  if (callback != NULL) {
-    _event_end = callback;
-  }
+    /**
+    * Set the event end callback function
+    * @param callback Function to call when an event ends
+    */
+    if (callback != NULL) {
+        _event_end = callback;
+    }
+}
+
+void Tripwire::reset_event_status() {
+    /**
+    * Reset the event status of the tripwire.
+    * This is a manual way of allowing new events to be detected in case something gets stuck.
+    */
+    _successive_detections = 0;
 }
--- Tripwire.h
+++ Tripwire.h
@@ -3,111 +3,111 @@
 
 #include <Arduino.h>
 
-typedef long (*range_function)(
-    void); /**< Passed function to get the range of the sensor */
-typedef void (*event_callback)(
-    void); /**< Callback function for sensor events */
+typedef long (*range_function)(void); /**< Passed function to get the range of the sensor */
+typedef void (*event_callback)(void); /**< Callback function for sensor events */
 
 class Tripwire {
-  /**
-  * Library to manage detection events for a ranging sensor
-  */
+    /**
+    * Library to manage detection events for a ranging sensor
+    */
 
-public:
-  /**
-  * Create a new tripwire object
-  * @param get_range Passed function to get the range of the function. Unitless,
-  * but must be a long.
-  */
-  Tripwire(range_function get_range = NULL);
+   public:
+    /**
+    * Create a new tripwire object
+    * @param get_range Passed function to get the range of the function. Unitless,
+    * but must be a long.
+    */
+    Tripwire(range_function get_range = NULL);
 
-  /**
-  * Start the tripwire.
-  * Runtime variables are reset.
-  * update must be called regularly to ensure consistent reads
-  * @param distance_threshold Minimum reduction in distance to count a
-  * detection. Units are inhereted from the _get_range function.
-  */
-  void start(long distance_threshold);
+    /**
+    * Start the tripwire.
+    * Runtime variables are reset.
+    * update must be called regularly to ensure consistent reads
+    * @param distance_threshold Minimum reduction in distance to count a
+    * detection. Units are inhereted from the _get_range function.
+    */
+    void start();
 
-  /**
-  * Calibrate the baseline distance of the sensor.
-  * Ensure nothing is moving in front of the sensor during calibration or the
-  * baseline will be inaccurate.
-  * is_calibrated will be updated after this function has completed to indicate
-  * whether the calibration was successful.
-  */
-  void calibrate();
+    /**
+    * Calibrate the baseline distance of the sensor.
+    * Ensure nothing is moving in front of the sensor during calibration or the
+    * baseline will be inaccurate.
+    * is_calibrated will be updated after this function has completed to indicate
+    * whether the calibration was successful.
+    */
+    void calibrate();
 
-  /**
-  * Update the state of the tripwire
-  * This function should be called regularly to ensure data is accurate.
-  * Event callbacks are called from this function.
-  */
-  void update();
+    /**
+    * Update the state of the tripwire
+    * This function should be called regularly to ensure data is accurate.
+    * Event callbacks are called from this function.
+    */
+    void update();
 
-  /**
-  * Set the _get_range function
-  * @param function The external function that returns the recorded distance of
-  * the sensor
-  */
-  void set_range_function(range_function function);
+    /**
+    * Set the _get_range function
+    * @param function The external function that returns the recorded distance of
+    * the sensor
+    */
+    void set_range_function(range_function function);
 
-  /**
-  * Set the event start callback function
-  * @param callback Function to call when an event starts
-  */
-  void set_event_start_callback(event_callback callback);
+    /**
+    * Set the event start callback function
+    * @param callback Function to call when an event starts
+    */
+    void set_event_start_callback(event_callback callback);
 
-  /**
-  * Set the event end callback function
-  * @param callback Function to call when an event ends
-  */
-  void set_event_end_callback(event_callback callback);
+    /**
+    * Set the event end callback function
+    * @param callback Function to call when an event ends
+    */
+    void set_event_end_callback(event_callback callback);
 
-  bool is_calibrated; /**< Indicator that the sensor is calibrated. True if
-                         calibrated*/
-  unsigned long num_detections; /**< Number of events detected by the sensor */
-  long distance; /**< Last distance measured by the sensor. Units inhereted */
-  long baseline_distance;  /**< Background range of the sensor */
-  long baseline_variance;  /**< Amount that the baseline varied during
-                              calibration. Units inhereted */
-  long distance_threshold; /**< Amount the distance must change to count as a
-                              detection. Units inhereted. */
-  int min_baseline_reads;  /**< The minimum number of baseline reads needed for
-                              calibration */
-  int max_baseline_reads;  /**< The maximum number of baseline reads needed for
-                              calibration */
-  long max_baseline_variance;    /**< The maximum allowed baseline variance.
-                                    Calibration will fail if the variance exceeds
-                                    this value. */
-  long baseline_read_interval;   /**< Amount of time between sensor reads during
-                                    calibration. [ms] */
-  int min_successive_detections; /**< Minimum number of successive detects
-                                    needed to trigger an event. */
-  unsigned long last_event_width; /**< Width of the last sensor event [ms] */
-  unsigned long
-      event_start_time; /**< Time that the last sensor event started [ms] */
+    /**
+    * Reset the event status of the tripwire.
+    * This is a manual way of allowing new events to be detected in case something gets stuck.
+    */
+    void reset_event_status();
 
-private:
-  event_callback _event_start; /**< Function to call when an event starts */
-  event_callback _event_end;   /**< Function to call when an event ends */
-  range_function _get_range;   /**< External function that returns the sensor's
-                                  reported distance */
-  int _successive_detections;  /**< The current number of successive detections
-                                  */
+    bool is_calibrated;             /**< Indicator that the sensor is calibrated. True if
+                                       calibrated*/
+    unsigned long num_detections;   /**< Number of events detected by the sensor */
+    long distance;                  /**< Last distance measured by the sensor. Units inhereted */
+    long baseline_distance;         /**< Background range of the sensor */
+    long baseline_variance;         /**< Amount that the baseline varied during
+                                       calibration. Units inhereted */
+    long distance_threshold;        /**< Amount the distance must change to count as a
+                                       detection. Units inhereted. */
+    int min_baseline_reads;         /**< The minimum number of baseline reads needed for
+                                       calibration */
+    int max_baseline_reads;         /**< The maximum number of baseline reads needed for
+                                       calibration */
+    long max_baseline_variance;     /**< The maximum allowed baseline variance.
+                                       Calibration will fail if the variance exceeds
+                                       this value. */
+    long baseline_read_interval;    /**< Amount of time between sensor reads during
+                                       calibration. [ms] */
+    int min_successive_detections;  /**< Minimum number of successive detects
+                                       needed to trigger an event. */
+    unsigned long last_event_width; /**< Width of the last sensor event [ms] */
+    unsigned long event_start_time; /**< Time that the last sensor event started [ms] */
 
-  const static long DEFAULT_DISTANCE_THRESHOLD =
-      70; /**< Default threshold distance [implied cm] */
-  const static int DEFAULT_MIN_BASELINE_READS =
-      20; /**< Default minimum number of baseline reads for calibration */
-  const static int DEFAULT_MAX_BASELINE_READS =
-      40; /**< Default maximum number of baseline reads for calibration */
-  const static long DEFAULT_MAX_BASELINE_VARIANCE =
-      DEFAULT_DISTANCE_THRESHOLD; /**< Default maximum allowed variance in the
-                                     baseline reading [cm] */
-  const static long DEFAULT_BASELINE_READ_INTERVAL =
-      100; /**< Default time between sensor reads during calibration [ms] */
-  const static int DEFAULT_MINIMUM_SUCCESSIVE_DETECTIONS =
-      0; /**< Default number of successive sensor detections */
-}
+   private:
+    event_callback _event_start; /**< Function to call when an event starts */
+    event_callback _event_end;   /**< Function to call when an event ends */
+    range_function _get_range;   /**< External function that returns the sensor's
+                                    reported distance */
+    int _successive_detections;  /**< The current number of successive detections
+                                    */
+
+    const static long DEFAULT_DISTANCE_THRESHOLD = 70; /**< Default threshold distance [implied cm] */
+    const static int DEFAULT_MIN_BASELINE_READS = 20;  /**< Default minimum number of baseline reads for calibration */
+    const static int DEFAULT_MAX_BASELINE_READS = 40;  /**< Default maximum number of baseline reads for calibration */
+    const static long DEFAULT_MAX_BASELINE_VARIANCE =
+        DEFAULT_DISTANCE_THRESHOLD; /**< Default maximum allowed variance in the
+                                       baseline reading [cm] */
+    const static long DEFAULT_BASELINE_READ_INTERVAL =
+        100; /**< Default time between sensor reads during calibration [ms] */
+    const static int DEFAULT_MINIMUM_SUCCESSIVE_DETECTIONS = 0; /**< Default number of successive sensor detections */
+};
+#endif
